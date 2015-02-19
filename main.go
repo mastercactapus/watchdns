c9088f79b5feed4462a2ae821928b410e6083fed
Nathan Caza <mastercactapus@gmail.com>
2015-02-18 22:38:42 -0600

    add flags and configuration

--- main.go
+++ main.go
@@ -1,14 +1,90 @@
 package main
 
 import (
+	"github.com/coreos/fleet/registry"
+	"github.com/miekg/dns"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"net"
+	"regexp"
+	"strings"
+	"time"
 )
 
 var mainCmd = &cobra.Command{
 	Use:   "watchdns",
 	Short: "A dynamic dns server configured by fleet service files",
+	Run:   execute,
+}
+
+var domainRx = regexp.MustCompile(`^[a-z][a-z0-9-.]*\.$`)
+
+func execute(cmd *cobra.Command, args []string) {
+	setupLogrus()
+	if !domainRx.MatchString(strings.ToLower(viper.GetString("WatchDomain"))) {
+		log.Fatalln("invalid domain specified for WatchDomain:", viper.GetString("WatchDomain"))
+	}
+	machines()
+}
+
+func init() {
+	mainCmd.PersistentFlags().StringP("watch-domain", "watchdns.", "tld to serve queries from, must end with a '.'")
+	mainCmd.PersistentFlags().Duration("check-interval", time.Second*5, "Interval to set for CheckInterval when unspecified in a unit file.")
+	mainCmd.PersistentFlags().Duration("check-timeout", time.Second*3, "Timeout for TCP and HTTP checks when unspecified in a unit file.")
+	mainCmd.PersistentFlags().UintP("check-concurrent", "c", 20, "Number of concurrent health checks to run.")
+	mainCmd.PersistentFlags().Duration("check-resolution", time.Second, "Maximum tick resolution for health check intervals.")
+	mainCmd.PersistentFlags().DurationP("fleet-interval", "i", time.Second*3, "Time to wait between polling fleet for service changes.")
+	mainCmd.PersistentFlags().StringP("etcd-peers", "e", "http://localhost:4001", "Comma-delimited list of etcd peers to connect to.")
+	mainCmd.PersistentFlags().Duration("etcd-timeout", time.Second*5, "Timeout for etcd operations to complete.")
+	mainCmd.PersistentFlags().String("fleet-prefix", registry.DefaultKeyPrefix, "Prefix for fleet registry in etcd.")
+	mainCmd.PersistentFlags().StringP("bind-address", "b", ":8053", "Bind address for the DNS responder.")
+	mainCmd.PersistentFlags().StringP("log-level", "l", "warn", "Log verbosity level, can be: 'debug', 'info', 'warn', 'error', or 'fatal'.")
+	mainCmd.PersistentFlags().StringP("log-format", "o", "ascii", "Log format, can be: 'ascii' or 'json'.")
+	viper.BindPFlag("WatchDomain", mainCmd.PersistentFlags().Lookup("watch-domain"))
+	viper.BindPFlag("CheckInterval", mainCmd.PersistentFlags().Lookup("check-interval"))
+	viper.BindPFlag("CheckTimeout", mainCmd.PersistentFlags().Lookup("check-timeout"))
+	viper.BindPFlag("CheckConcurrent", mainCmd.PersistentFlags().Lookup("check-concurrent"))
+	viper.BindPFlag("CheckResolution", mainCmd.PersistentFlags().Lookup("check-resolution"))
+	viper.BindPFlag("FleetInterval", mainCmd.PersistentFlags().Lookup("fleet-interval"))
+	viper.BindPFlag("EtcdTimeout", mainCmd.PersistentFlags().Lookup("etcd-timeout"))
+	viper.BindPFlag("EtcdPeers", mainCmd.PersistentFlags().Lookup("etcd-peers"))
+	viper.BindPFlag("FleetPrefix", mainCmd.PersistentFlags().Lookup("fleet-prefix"))
+	viper.BindPFlag("BindAddress", mainCmd.PersistentFlags().Lookup("bind-address"))
+	viper.BindPFlag("LogLevel", mainCmd.PersistentFlags().Lookup("log-level"))
+	viper.BindPFlag("LogFormat", mainCmd.PersistentFlags().Lookup("log-format"))
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/watchdns/")
+	viper.ReadInConfig()
+	viper.SetEnvPrefix("wdns")
+	viper.AutomaticEnv()
+}
+
+func setupLogrus() {
+	switch viper.GetString("LogFormat") {
+	case "ascii":
+		log.SetFormatter(&log.TextFormatter{})
+	case "json":
+		log.SetFormatter(&log.JSONFormatter{})
+	default:
+		log.Fatalln("Unknown log format:", viper.GetString("LogFormat"))
+	}
+	switch viper.GetString("LogLevel") {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "fatal":
+		log.SetLevel(log.FatalLevel)
+	default:
+		log.Fatalln("Unknown log level:", viper.GetString("LogLevel"))
+	}
 }
 
 func main() {
-
+	mainCmd.Execute()
 }
